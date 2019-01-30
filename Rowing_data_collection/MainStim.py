ca2d27860f75b7a6ad1bd6d9cc56833dfa46274a
Lucas Fonseca <lucasfonseca@lara.unb.br>
2019-01-30 11:33:37 -0200

    small change on running method in the button "mainstim" script

--- Rowing_data_collection/MainStim.py
+++ Rowing_data_collection/MainStim.py
@@ -255,7 +255,7 @@ def running(current_CH12, current_CH34, current_CH56, current_CH78, pw, mode, th
             # colocando-se pw no canal que se quer estimular
         if stimulation:
             stim.update(this_channels, pw_str, current_str)
-        # print("Flexao")
+        print(stim_state)
         if connection:
             # server.send(dict({'state':'Flexão', 'current':current_str}))
             server.send([time.time(), stim_state, current_str])
